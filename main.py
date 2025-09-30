90f12de67f64aa493b369f0e1d54ecd70ac203b3
rev3nt <mikhail.merkulov.2005@mail.ru>
2025-09-30 23:17:25 +0300

    The architecture of the To-Do list has been changed from a dictionary to a list

--- main.py
+++ main.py
@@ -1,27 +1,44 @@
 class ToDoList:
     def __init__(self):
-        self.todoList = {}
+        self.todoList = []
 
 
     def add_task(self, task):
-        self.todoList[task] = {'completed': False}
+        self.todoList.append({'name': task,'completed': False})
 
 
-    def complete_task(self, task):
-        self.todoList[task]['completed'] = True
+    def complete_task(self, task_name):
+        for task in self.todoList:
+            if task['name'] == task_name:
+                task['completed'] = True
+
+                return
+
+        print("Таска нет в списке")
 
 
-    def remove_task(self, task):
-        del self.todoList[task]
+    def remove_task(self, task_name):
+        for task in self.todoList:
+            if task['name'] == task_name:
+                self.todoList.remove(task)
+
+                return
+
+        print("Таска нет в списке")
 
 
     def list_tasks(self):
-        for task in self.todoList:
-            print(f'{task} {'✅' if self.todoList[task]["completed"] else '❌'}')
-        print()
+        if len(self.todoList) != 0:
+            for task in self.todoList:
+                print(f'{task["name"]} {"✅" if task["completed"] else "❌"}')
+            print()
+        else:
+            print('Список пуст')
 
 
 todo = ToDoList()
+
+todo.list_tasks()
 
 todo.add_task('Заняться пайтоном')
 
