5eedbb536625f8feaeac0b629de5c28b5f73cd55
rev3nt <mikhail.merkulov.2005@mail.ru>
2025-10-05 11:31:45 +0300

    Back to dict

--- main.py
+++ main.py
@@ -1,55 +1,63 @@
 class ToDoList:
     def __init__(self):
-        self.todoList = []
+        self.todoList = {}
 
 
     def add_task(self, task):
-        self.todoList.append({'name': task,'completed': False})
+        self.todoList[task] = {'completed': False}
 
 
     def complete_task(self, task_name):
-        for task in self.todoList:
-            if task['name'] == task_name:
-                task['completed'] = True
+        if task_name in self.todoList:
+            self.todoList[task_name]['completed'] = True
 
-                return
+            return
 
         print("Таска нет в списке")
 
 
     def remove_task(self, task_name):
-        for task in self.todoList:
-            if task['name'] == task_name:
-                self.todoList.remove(task)
+        if task_name in self.todoList:
+            del self.todoList[task_name]
 
-                return
+            return
 
         print("Таска нет в списке")
 
 
     def list_tasks(self):
-        if len(self.todoList) != 0:
-            for task in self.todoList:
-                print(f'{task["name"]} {"✅" if task["completed"] else "❌"}')
+        if self.todoList:
+            for task, task_status in self.todoList.items():
+                print(f'{task} {"✅" if task_status["completed"] else "❌"}')
             print()
         else:
             print('Список пуст')
 
 
+options = '''\n1. Вывести таски.
+2. Добавить таск.
+3. Отметить таск, как выполненный.
+4. Удалить таск.
+5. Выйти из программы\n'''
+
 todo = ToDoList()
 
-todo.list_tasks()
+func_dict = {
+    '1': lambda: todo.list_tasks(),
+    '2': lambda: todo.add_task(input('Введите название добавляемого таска: ')),
+    '3': lambda: todo.complete_task(input('Введите название выполненного таска: ')),
+    '4': lambda: todo.remove_task(input('Введите название удаляемого таска: ')),
+}
 
-todo.add_task('Заняться пайтоном')
+while True:
+    print(options)
 
-todo.add_task('Потрогать траву')
+    user_input = input('Введите опцию из меню: ').strip()
 
-todo.list_tasks()
+    if user_input == '5':
+        break
 
-todo.complete_task('Заняться пайтоном')
-
-todo.list_tasks()
-
-todo.remove_task('Потрогать траву')
-
-todo.list_tasks()
+    if user_input in func_dict:
+        func_dict[user_input]()
+    else:
+        print('Введите номер опции!')
